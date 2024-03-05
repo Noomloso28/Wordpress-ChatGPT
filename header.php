bc18e35841fadc99884377cbc5c7aea309daba17
K.Noom <noom@immerce.de>
2024-03-05 14:40:24 +0700

    Wordpress autopost by Chatgpt

--- header.php
+++ header.php
@@ -6,7 +6,6 @@ error_reporting(0);
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
 <nav class="navbar navbar-expand-lg bg-light">
 	<div class="container-fluid">
 		<a class="navbar-brand" href="?page=chatgpt-content-writer-dashboard">ChatGPT Content Writer</a>
